b0c33f64ac8edb0bb112a2e324d1ed7327baf533
Robert Raver <raverrr@amazon.com>
2024-11-13 11:17:39 -0800

    opensearch deployment and vpc

--- medialake_constructs/shared_constructs/opensearch_managed_cluster.py
+++ medialake_constructs/shared_constructs/opensearch_managed_cluster.py
@@ -7,6 +7,23 @@ from aws_cdk import (
 )
 from constructs import Construct
 from typing import Optional, List, Dict
+from dataclasses import dataclass
+
+
+@dataclass
+class OpenSearchClusterProps:
+    domain_name: str
+    engine_version: str = "OpenSearch_1.3"
+    instance_type: str = "r6g.large.search"
+    instance_count: int = 3
+    volume_size: int = 20
+    availability_zone_count: int = 3
+    vpc: Optional[ec2.IVpc] = None
+    enforce_https: bool = True
+    node_to_node_encryption: bool = True
+    encryption_at_rest: bool = True
+    master_node_instance_type: str = "r6g.large.search"
+    master_node_count: int = 2
 
 
 class OpenSearchCluster(Construct):
@@ -15,16 +32,7 @@ class OpenSearchCluster(Construct):
         scope: Construct,
         id: str,
         *,
-        domain_name: str,
-        engine_version: str = "OpenSearch_1.3",
-        instance_type: str = "r6g.large.search",
-        instance_count: int = 3,
-        volume_size: int = 20,
-        availability_zone_count: int = 3,
-        vpc: Optional[ec2.IVpc] = None,
-        enforce_https: bool = True,
-        node_to_node_encryption: bool = True,
-        encryption_at_rest: bool = True
+        props: OpenSearchClusterProps,
     ) -> None:
         super().__init__(scope, id)
 
@@ -32,24 +40,27 @@ class OpenSearchCluster(Construct):
         self.domain = opensearch.Domain(
             self,
             "OpenSearchDomain",
-            domain_name=domain_name,
-            version=opensearch.EngineVersion.of(engine_version),
+            domain_name=props.domain_name,
+            version=opensearch.EngineVersion.OPENSEARCH_2_11,
             # Capacity configuration
             capacity={
-                "data_nodes": instance_count,
-                "data_node_instance_type": instance_type,
+                "data_nodes": props.instance_count,
+                "data_node_instance_type": props.instance_type,
+                # "master_nodes": props.master_node_count,
+                # "master_node_instance_type": props.master_node_instance_type,
+                "multi_az_with_standby_enabled": False,
             },
             # EBS configuration
             ebs={
-                "volume_size": volume_size,
+                "volume_size": props.volume_size,
                 "volume_type": ec2.EbsDeviceVolumeType.GP3,
             },
             # Zone awareness configuration
-            zone_awareness={"availability_zone_count": availability_zone_count},
+            # zone_awareness={"availability_zone_count": props.availability_zone_count},
             # Security configuration
-            enforce_https=enforce_https,
-            node_to_node_encryption=node_to_node_encryption,
-            encryption_at_rest={"enabled": encryption_at_rest},
+            enforce_https=props.enforce_https,
+            # node_to_node_encryption=props.node_to_node_encryption,
+            # encryption_at_rest={"enabled": props.encryption_at_rest},
             # Logging configuration
             logging={
                 "slow_search_log_enabled": True,
@@ -57,12 +68,19 @@ class OpenSearchCluster(Construct):
                 "slow_index_log_enabled": True,
             },
             # VPC configuration
-            vpc=vpc,
-            vpc_subnets=(
-                vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)
-                if vpc
-                else None
-            ),
+            # vpc=props.vpc,
+            # vpc_subnets=(
+            #     [
+            #         ec2.SubnetSelection(
+            #             subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+            #             availability_zones=props.vpc.availability_zones[
+            #                 : props.availability_zone_count
+            #             ],
+            #         )
+            #     ]
+            #     if props.vpc
+            #     else None
+            # ),
             # Advanced options
             advanced_options={
                 "rest.action.multi.allow_explicit_index": "true",
--- medialake_constructs/vpc.py
+++ medialake_constructs/vpc.py
@@ -7,21 +7,21 @@ from aws_cdk import (
 )
 from constructs import Construct
 from typing import Optional, List, Dict
+from dataclasses import dataclass
+
+
+@dataclass
+class CustomVpcProps:
+    vpc_name: str
+    max_azs: int = 3
+    nat_gateways: int = 1
+    cidr: str = "10.0.0.0/16"
+    enable_dns_hostnames: bool = True
+    enable_dns_support: bool = True
 
 
 class CustomVpc(Construct):
-    def __init__(
-        self,
-        scope: Construct,
-        id: str,
-        *,
-        vpc_name: str,
-        max_azs: int = 3,
-        nat_gateways: int = 1,
-        cidr: str = "10.0.0.0/16",
-        enable_dns_hostnames: bool = True,
-        enable_dns_support: bool = True
-    ) -> None:
+    def __init__(self, scope: Construct, id: str, *, props: CustomVpcProps) -> None:
         super().__init__(scope, id)
 
         # Default subnet configuration
@@ -45,43 +45,43 @@ class CustomVpc(Construct):
         self.vpc = ec2.Vpc(
             self,
             "CustomVPC",
-            vpc_name=vpc_name,
-            max_azs=max_azs,
-            nat_gateways=nat_gateways,
-            ip_addresses=ec2.IpAddresses.cidr(cidr),
-            enable_dns_hostnames=enable_dns_hostnames,
-            enable_dns_support=enable_dns_support,
+            vpc_name=props.vpc_name,
+            max_azs=props.max_azs,
+            nat_gateways=props.nat_gateways,
+            ip_addresses=ec2.IpAddresses.cidr(props.cidr),
+            enable_dns_hostnames=props.enable_dns_hostnames,
+            enable_dns_support=props.enable_dns_support,
             subnet_configuration=subnet_configuration,
             gateway_endpoints={
-                "S3": ec2.GatewayVpcEndpointAwsService.S3,
-                "DynamoDB": ec2.GatewayVpcEndpointAwsService.DYNAMODB,
+                "S3": {"service": ec2.GatewayVpcEndpointAwsService.S3},
+                "DynamoDB": {"service": ec2.GatewayVpcEndpointAwsService.DYNAMODB},
             },
         )
 
         # Add interface endpoints
-        self._add_interface_endpoints()
+        # self._add_interface_endpoints()
 
         self.vpc_id = self.vpc.vpc_id
 
-    def _add_interface_endpoints(self) -> None:
-        """Add VPC interface endpoints for common AWS services"""
-        self.vpc.add_interface_endpoint(
-            "ECRDockerEndpoint", service=ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER
-        )
+    # def _add_interface_endpoints(self) -> None:
+    # """Add VPC interface endpoints for common AWS services"""
+    # self.vpc.add_interface_endpoint(
+    #     "ECRDockerEndpoint", service=ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER
+    # )
 
-        self.vpc.add_interface_endpoint(
-            "ECREndpoint", service=ec2.InterfaceVpcEndpointAwsService.ECR
-        )
+    # self.vpc.add_interface_endpoint(
+    #     "ECREndpoint", service=ec2.InterfaceVpcEndpointAwsService.ECR
+    # )
 
-        self.vpc.add_interface_endpoint(
-            "CloudWatchLogsEndpoint",
-            service=ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
-        )
+    # self.vpc.add_interface_endpoint(
+    #     "CloudWatchLogsEndpoint",
+    #     service=ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+    # )
 
-        self.vpc.add_interface_endpoint(
-            "SecretsManagerEndpoint",
-            service=ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
-        )
+    # self.vpc.add_interface_endpoint(
+    #     "SecretsManagerEndpoint",
+    #     service=ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+    # )
 
     def get_subnet_ids(self, subnet_type: ec2.SubnetType) -> List[str]:
         """Get subnet IDs for a specific subnet type"""
