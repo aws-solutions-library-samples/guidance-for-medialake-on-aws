9b7e8aa4b5008fb5e73c163ed4bf53835b603480
Robert Raver <raverrr@amazon.com>
2024-11-15 15:14:46 -0800

    commented out fake routes

--- medialake_user_interface/src/routes.tsx
+++ medialake_user_interface/src/routes.tsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import { RouteObject } from 'react-router-dom';
-import AssetsPage from './pages/AssetsPage';
-import SearchPage from './pages/SearchPage';
-import PipelinesPage from './pages/PipelinesPage';
-import ExecutionsPage from './pages/ExecutionsPage';
-import ReviewQueuePage from './pages/ReviewQueuePage';
-import TagsPage from './pages/TagsPage';
-import SettingsComponent from './SettingsComponent';
+// import React from 'react';
+// import { RouteObject } from 'react-router-dom';
+// import AssetsPage from './pages/AssetsPage';
+// import SearchPage from './pages/SearchPage';
+// import PipelinesPage from './pages/PipelinesPage';
+// import ExecutionsPage from './pages/ExecutionsPage';
+// import ReviewQueuePage from './pages/ReviewQueuePage';
+// import TagsPage from './pages/TagsPage';
+// import SettingsComponent from './SettingsComponent';
 
-const routes: RouteObject[] = [
-    {
-        index: true,
-        element: <AssetsPage />,
-    },
-    {
-        path: 'search',
-        element: <SearchPage />,
-    },
-    {
-        path: 'pipelines',
-        element: <PipelinesPage />,
-    },
-    {
-        path: 'executions',
-        element: <ExecutionsPage />,
-    },
-    {
-        path: 'review-queue',
-        element: <ReviewQueuePage />,
-    },
-    {
-        path: 'tags',
-        element: <TagsPage />,
-    },
-    {
-        path: 'settings',
-        element: <SettingsComponent />,
-    },
-];
+// const routes: RouteObject[] = [
+//     {
+//         index: true,
+//         element: <AssetsPage />,
+//     },
+//     {
+//         path: 'search',
+//         element: <SearchPage />,
+//     },
+//     {
+//         path: 'pipelines',
+//         element: <PipelinesPage />,
+//     },
+//     {
+//         path: 'executions',
+//         element: <ExecutionsPage />,
+//     },
+//     {
+//         path: 'review-queue',
+//         element: <ReviewQueuePage />,
+//     },
+//     {
+//         path: 'tags',
+//         element: <TagsPage />,
+//     },
+//     {
+//         path: 'settings',
+//         element: <SettingsComponent />,
+//     },
+// ];
 
-export default routes;
+// export default routes;
