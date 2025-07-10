2692fe82a6c2e496b14fbd362198df35dfbc7e2b
Robert Raver <raverrr@amazon.com>
2025-07-10 13:21:56 -0700

    fix: update to cicd templates for github source cicd source

---
+++ .cicd/build_ffmpeg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASE_DIR=$(pwd)
+TEMP_DIR=$(mktemp -d)
+cd $TEMP_DIR
+curl https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg-release-amd64-static.tar.xz
+curl https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz.md5 -o ffmpeg-release-amd64-static.tar.xz.md5
+md5sum -c ffmpeg-release-amd64-static.tar.xz.md5
+mkdir ffmpeg-release-amd64
+tar xvf ffmpeg-release-amd64-static.tar.xz -C ffmpeg-release-amd64
+mkdir -p $BASE_DIR/dist/lambdas/layers/ffmpeg/bin
+cp ffmpeg-release-amd64/*/ffmpeg $BASE_DIR/dist/lambdas/layers/ffmpeg/bin
+cd $BASE_DIR
+rm -rf $TEMP_DIR
